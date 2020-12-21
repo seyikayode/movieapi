278bf85c0e509e5a976aba5be7b85e7b3ee3ea4c
Seyikayode <oluwaseyikayode25@gmail.com>
2020-12-21 05:12:21 -0800

    docs

--- movieapi/urls.py
+++ movieapi/urls.py
@@ -17,10 +17,27 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Video API",
+      default_version='v1',
+      description="Video API endpoints",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("movie.urls"))
+    path("", include("movie.urls")),
+    path('api/v1/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
