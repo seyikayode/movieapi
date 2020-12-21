c1d79c50ba6c3d9ee0026053fa2a06443558d78d
Seyikayode <oluwaseyikayode25@gmail.com>
2020-12-21 06:50:33 -0800

    done

--- movie/urls.py
+++ movie/urls.py
@@ -4,9 +4,15 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("api/movie/", views.MovieTypeList.as_view()),
-    path("api/movie/<int:pk>/", views.MediaTypeList.as_view()),
-    path("api/movie/<int:pk>/video/<int:pk2>/", views.VideoList.as_view())
+    path("api/movies/", views.MovieTypeList.as_view()),
+    path("api/add_movie/", views.MovieTypeCreate.as_view()),
+    path("api/movies/<int:pk>/", views.MovieTypeDetail.as_view()),
+    path("api/movies/<int:pk>/medias/", views.MediaTypeList.as_view()),
+    path("api/movies/<int:pk>/add_media/", views.MediaTypeCreate.as_view()),
+    path("api/movies/<int:pk>/medias/<int:media_pk>/", views.MediaTypeDetail.as_view()),
+    path("api/movies/<int:pk>/medias/<int:media_pk>/videos/", views.VideoList.as_view()),
+    path("api/movies/<int:pk>/medias/<int:media_pk>/add_video/", views.VideoCreate.as_view()),
+    path("api/movies/<int:pk>/medias/<int:media_pk>/videos/<int:video_pk>/", views.VideoDetail.as_view())
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- movie/views.py
+++ movie/views.py
@@ -14,7 +14,9 @@ class MovieTypeList(APIView):
         types = MovieType.objects.all()
         data = MovieTypeSerializer(types, many=True).data
         return Response(data)
-    
+
+
+class MovieTypeCreate(APIView):
     def post(self, request, *args, **kwargs):
         movie_serializer = MovieTypeSerializer(data=request.data)
         if movie_serializer.is_valid():
@@ -24,13 +26,22 @@ class MovieTypeList(APIView):
             return Response(movie_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class MovieTypeDetail(APIView):
+    def get(self, request, pk):
+        types = get_object_or_404(MovieType, pk=pk)
+        data = MovieTypeSerializer(types).data
+        return Response(data)
+
+
 class MediaTypeList(APIView):
     def get(self, request, pk):
         types = get_object_or_404(MovieType, pk=pk)
         media = MediaType.objects.filter(movie_type_id=types)
         data = MediaTypeSerializer(media, many=True).data
         return Response(data)
-    
+
+
+class MediaTypeCreate(APIView):
     def post(self, request, pk):
         # types = get_object_or_404(MovieType, pk=pk)
         movie_name = request.data.get('movie_name')
@@ -47,24 +58,34 @@ class MediaTypeList(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class VideoList(APIView):
-    parser_classes = (MultiPartParser,)
-
-    def get(self, request, pk, pk2):
+class MediaTypeDetail(APIView):
+    def get(self, request, pk, media_pk):
         types = get_object_or_404(MovieType, pk=pk)
-        media = get_object_or_404(MediaType, movie_type_id=types, pk=pk2)
+        media = MediaType.objects.filter(movie_type_id=types, pk=media_pk)
+        data = MediaTypeSerializer(media, many=True).data
+        return Response(data)
+
+
+class VideoList(APIView):
+    def get(self, request, pk, media_pk):
+        types = get_object_or_404(MovieType, pk=pk)
+        media = get_object_or_404(MediaType, movie_type_id=types, pk=media_pk)
         video = Videos.objects.filter(media_type_id=media)
         data = VideosSerializer(video, many=True).data
         return Response(data)
-    
-    def post(self, request, pk, pk2):
+
+
+class VideoCreate(APIView):
+    parser_classes = (MultiPartParser,)
+
+    def post(self, request, pk, media_pk):
         # types = get_object_or_404(MovieType, pk=pk)
         # media = get_object_or_404(MediaType, movie_type_id=types, pk=pk2)
         episode = request.data.get('episode')
         description = request.data.get('description')
         thumbnail = request.data.get('thumbnail')
         video = request.data.get('video')
-        data = {'movie_type': pk, 'media_type': pk2, 'episode': episode, 'description': description,
+        data = {'movie_type': pk, 'media_type': media_pk, 'episode': episode, 'description': description,
                'thumbnail': thumbnail, 'video': video}
         video_serializer = VideosSerializer(data=data)
         if video_serializer.is_valid():
@@ -72,4 +93,19 @@ class VideoList(APIView):
             return Response(video_serializer.data, status=status.HTTP_201_CREATED)
         else: 
             return Response(video_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+
+class VideoDetail(APIView):
+    def get(self, request, pk, media_pk, video_pk):
+        types = get_object_or_404(MovieType, pk=pk)
+        media = get_object_or_404(MediaType, movie_type_id=types, pk=media_pk)
+        video = Videos.objects.filter(media_type_id=media, pk=video_pk)
+        data = VideosSerializer(video, many=True).data
+        return Response(data)
+
+# class MediaTypeDetail(APIView):
+#     def get(self, request, pk, media_pk):
+#         types = get_object_or_404(MovieType, pk=pk)
+#         media = MediaType.objects.filter(movie_type_id=types, pk=media_pk)
+#         data = MediaTypeSerializer(media, many=True).data
+#         return Response(data)
